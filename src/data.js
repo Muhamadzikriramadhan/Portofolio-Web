83185b76ce453793756b1dfe4f850fa690424a0b
Muhamadzikriramadhan <ramadhanmzikri@gmail.com>
2023-02-04 17:50:47 +0700

    first_comment

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "SiPointMart",
+    subtitle: "PPOB Apps",
+    description:
+      "This is a personal PPOB application, which is complete with the remittance feature.",
+    image: "./pr1.gif",
+    link: "https://github.com/Muhamadzikriramadhan",
+  },
+  {
+    title: "Zafa POS",
+    subtitle: "Caffe Apps",
+    description:
+      "This is an app that is still a work in progress, it is built with Aziz, Zikri, Arjuna and Alifa.",
+    image: "./pr2.gif",
+    link: "https://github.com/Muhamadzikriramadhan",
+  },
+  {
+    title: "Travel Tele",
+    subtitle: "Travel & Hotel Apps",
+    description:
+      "This is a hotel booking application in Indonesia and booking tourist tickets in certain areas.",
+    image: "./pr3.gif",
+    link: "https://github.com/Muhamadzikriramadhan",
+  },
+  {
+    title: "SiPoint",
+    subtitle: "PPOB Apps",
+    description:
+      "This is a general PPOB application, which is equipped with a remittance feature, and all payment services.",
+    image: "./pr4.gif",
+    link: "https://github.com/Muhamadzikriramadhan",
+  },
+];
+
+export const skills = [
+  "PHP",
+  "Java",
+  "Kotlin",
+  "Github",
+  "MySQL",
+  "C++",
+  "Dart"
+];
